fb0a39191fbb5c3cf0abc1f47b04b97c46e8287d
Brandon Golway <brando56894@gmail.com>
2016-06-19 23:36:54 -0400

    initial commit

---
+++ weapons.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python2
+#~~weapons.py~~
+
+class create(object):
+    def find_weapon(self):
+        #TODO select a random weapon from that list and add it to the player's inventory'
+        available = {
+            "dagger": 5,
+            "sword": 25,
+            "pistol": 50            
+            }
+        print "\nYou found a weapon!"
+        sleep(2)
+        return self 
